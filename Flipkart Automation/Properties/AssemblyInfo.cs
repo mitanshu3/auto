ed5648e7b33055d8e1c8f893aafc7268d6c56e36
Mitanshu Shrivastava <mitanshu.shrivastava@outlook.com>
2018-03-24 13:06:05 +0530

    Initial Commit

---
+++ Flipkart Automation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Flipkart Automation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Flipkart Automation")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c6a5a53b-ef50-436b-85ea-7d54a25b5ce0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Flipkart Automation/TestHome.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Flipkart_Automation
+{
+    [TestClass]
+    public class TestHome
+    {
+        IWebDriver browseFlipkart;
+        string urlFlipkart;
+        IWebElement searchInputBox;
+        [TestInitialize]
+        public void TestInit()
+        {
+            urlFlipkart = "https://www.flipkart.com";
+            browseFlipkart = new ChromeDriver();
+        }
+        [TestMethod]
+        public void CheckHome()
+        {
+            browseFlipkart.Navigate().GoToUrl(urlFlipkart);
+            searchInputBox = browseFlipkart.FindElement(By.XPath("//*[@id=\"container\"]/div/header/div[1]/div[2]/div/div/div[2]/form/div/div[1]/div/input"));
+            searchInputBox.SendKeys("Saree"+Keys.Enter);
+        }
+        [TestCleanup]
+        public void CloseTest()
+        {
+
+        }
+    }
+}
